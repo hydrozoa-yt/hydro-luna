6d85c93d2ae57b6f955cfc426f2835cbe3f8bf4f
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-09 00:14:47 +0200

    make use of FishingSpot.kt

--- src/main/kotlin/world/location/draynorVillage/draynorVillage.kts
+++ src/main/kotlin/world/location/draynorVillage/draynorVillage.kts
@@ -2,8 +2,10 @@ package world.location.draynorVillage
 
 import api.predef.*
 import api.predef.ext.*
+import com.google.common.collect.ImmutableList
 import io.luna.game.event.impl.ServerStateChangedEvent.ServerLaunchEvent
 import io.luna.game.model.*
+import world.player.skill.fishing.catchFish.*
 import java.util.*
 
 on(ServerLaunchEvent::class) {
@@ -17,6 +19,6 @@ on(ServerLaunchEvent::class) {
     world.addNpc(174, 3084, 3235)
 
     // Fishing spots for shrimp
-    world.addNpc(316, 3085, 3230)
-    world.addNpc(316, 3086, 3227)
+    world.addNpc(FishingSpot(316, Position(3085, 3230), ImmutableList.of(Position(3085, 3229))))
+    world.addNpc(FishingSpot(316, Position(3086, 3227), ImmutableList.of(Position(3086, 3223))))
 }
