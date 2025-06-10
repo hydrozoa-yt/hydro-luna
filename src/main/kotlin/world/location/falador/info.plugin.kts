45a6d3cbaa20ed3cb8ad46aac8990ee198a77db5
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-10 20:10:18 +0200

    Add bankers and some shops to Falador

---
+++ src/main/kotlin/world/location/falador/info.plugin.kts
@@ -0,0 +1,13 @@
+package world.location.falador
+
+import api.plugin.dsl.plugin
+
+plugin {
+    name = "Falador"
+    description =
+        """
+        A plugin that populates Falador with NPCs.
+        """
+    version = "1.0"
+    authors += "hydrozoa"
+}
