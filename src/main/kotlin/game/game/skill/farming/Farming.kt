b95381d829a398987e672d179a7d441f83beaa38
hydrozoa <hydrozoa.rs@gmail.com>
2026-05-04 23:46:26 +0200

    use VarpType

--- src/main/kotlin/game/game/skill/farming/Farming.kt
+++ src/main/kotlin/game/game/skill/farming/Farming.kt
@@ -2,6 +2,7 @@ package game.skill.farming
 
 import api.attr.*
 import game.skill.farming.patch.*
+import io.luna.game.model.def.VarpDefinition
 import io.luna.game.model.mob.*
 import io.luna.game.model.mob.varp.*
 
@@ -33,7 +34,6 @@ object Farming {
         plr.herbPatches.values.forEach { patch ->
             herbState += patch.getVarpValue()
         }
-        plr.sendVarp(Varp(515, herbState))
+        plr.sendVarp(Varp(VarpDefinition.VarpType.HERB_PATCH.id, herbState))
     }
-
 }
